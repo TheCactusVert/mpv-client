af003856cfdcb961b417f24f47e8165b8deaf105
TheCactusVert <kevin@gavrois.fr>
2024-08-25 13:26:20 +0200

    chore: add deprecated events

--- src/ffi.rs
+++ src/ffi.rs
@@ -42,6 +42,8 @@ pub enum mpv_event_id {
     START_FILE = 6,
     END_FILE = 7,
     FILE_LOADED = 8,
+    IDLE = 11, // Deprecated
+    TICK = 14, // Deprecated
     CLIENT_MESSAGE = 16,
     VIDEO_RECONFIG = 17,
     AUDIO_RECONFIG = 18,
